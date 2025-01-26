d3ae60a1b2ad805a6744006dc097fdb3bd0170db
YadavDipak <yadavdipak2911@gmail.com>
2025-01-26 19:55:39 +0530

    All boards manage file

---
+++ src/component/HomePage/Boards.jsx
@@ -0,0 +1,22 @@
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+const boardStyles = {
+  backgroundColor: "#6495ED",
+  height: 100,
+  width: 200,
+  borderRadius: 2,
+  m: 4,
+};
+
+function Boards({ boardInfo }) {
+  return (
+    <Link to={`/boards/${boardInfo.id}`} key={boardInfo.id}>
+      <Button variant="contained" sx={boardStyles}>
+        {boardInfo.name}
+      </Button>
+    </Link>
+  );
+}
+
+export default Boards;
