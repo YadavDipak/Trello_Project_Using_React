71db9b2edb77bf0bd09e4138efdd1ea577e36903
YadavDipak <yadavdipak2911@gmail.com>
2025-01-31 23:20:12 +0530

    Fetching Api

--- src/services/FetchApi.js
+++ src/services/FetchApi.js
@@ -1,8 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import axios from "axios";
-
-const keyValue = import.meta.env.VITE_TRELLO_API_KEY;
-const tokenValue = import.meta.env.VITE_TRELLO_API_TOKEN;
+import { URL, keyValue, tokenValue } from "../config/config";
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
@@ -15,11 +13,10 @@ function checkStatus(response) {
 export async function FetchApi(boardId) {
   try {
     const response = await axios.get(
-      `https://api.trello.com/1/boards/${boardId}/lists?key=${keyValue}&token=${tokenValue}`
+      `${URL}/boards/${boardId}/lists?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("There was an error fetching the board!", error);
     throw error;
   }
 }
@@ -27,11 +24,10 @@ export async function FetchApi(boardId) {
 export async function getAllBoard() {
   try {
     const response = await axios.get(
-      `https://api.trello.com/1/members/me/boards?key=${keyValue}&token=${tokenValue}`
+      `${URL}/members/me/boards?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("There was an error fetching all boards!", error);
     throw error;
   }
 }
@@ -39,11 +35,10 @@ export async function getAllBoard() {
 export async function createBoard(nameOfBoard) {
   try {
     const response = await axios.post(
-      `https://api.trello.com/1/boards/?name=${nameOfBoard}&key=${keyValue}&token=${tokenValue}`
+      `${URL}/boards/?name=${nameOfBoard}&key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("There was an error creating the board!", error);
     throw error;
   }
 }
@@ -51,11 +46,10 @@ export async function createBoard(nameOfBoard) {
 export async function getCards(listId) {
   try {
     const response = await axios.get(
-      `https://api.trello.com/1/lists/${listId}/cards?key=${keyValue}&token=${tokenValue}`
+      `${URL}/lists/${listId}/cards?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error getting card info:", error.message);
     throw error;
   }
 }
@@ -63,12 +57,11 @@ export async function getCards(listId) {
 export async function createCards(cardName, listId) {
   try {
     const response = await axios.post(
-      `https://api.trello.com/1/cards?key=${keyValue}&token=${tokenValue}`,
+      `${URL}/cards?key=${keyValue}&token=${tokenValue}`,
       { name: cardName, idList: listId }
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("There was an error creating the card!", error);
     throw error;
   }
 }
@@ -76,12 +69,11 @@ export async function createCards(cardName, listId) {
 export async function createLists(listName, boardId) {
   try {
     const response = await axios.post(
-      `https://api.trello.com/1/lists?key=${keyValue}&token=${tokenValue}`,
+      `${URL}/lists?key=${keyValue}&token=${tokenValue}`,
       { name: listName, idBoard: boardId }
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error creating the list!", error.message);
     throw error;
   }
 }
@@ -89,12 +81,11 @@ export async function createLists(listName, boardId) {
 export async function deleteList(listId) {
   try {
     const response = await axios.put(
-      `https://api.trello.com/1/lists/${listId}/closed?key=${keyValue}&token=${tokenValue}`,
+      `${URL}/lists/${listId}/closed?key=${keyValue}&token=${tokenValue}`,
       { value: true }
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error deleting the list!", error.message);
     throw error;
   }
 }
@@ -102,11 +93,10 @@ export async function deleteList(listId) {
 export async function deleteCard(cardid) {
   try {
     const response = await axios.delete(
-      `https://api.trello.com/1/cards/${cardid}?key=${keyValue}&token=${tokenValue}`
+      `${URL}/cards/${cardid}?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error deleting the card!", error.message);
     throw error;
   }
 }
@@ -114,11 +104,10 @@ export async function deleteCard(cardid) {
 export async function getCheckList(cardId) {
   try {
     const response = await axios.get(
-      `https://api.trello.com/1/cards/${cardId}/checklists?key=${keyValue}&token=${tokenValue}`
+      `${URL}/cards/${cardId}/checklists?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error getting the checklist!", error.message);
     throw error;
   }
 }
@@ -126,12 +115,11 @@ export async function getCheckList(cardId) {
 export async function createCheckList(cardId, checkListName) {
   try {
     const response = await axios.post(
-      `https://api.trello.com/1/checklists?idCard=${cardId}&key=${keyValue}&token=${tokenValue}`,
+      `${URL}/checklists?idCard=${cardId}&key=${keyValue}&token=${tokenValue}`,
       { name: checkListName }
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error creating the checklist!", error.message);
     throw error;
   }
 }
@@ -139,11 +127,10 @@ export async function createCheckList(cardId, checkListName) {
 export async function deleteCheckList(checkListId) {
   try {
     const response = await axios.delete(
-      `https://api.trello.com/1/checklists/${checkListId}?key=${keyValue}&token=${tokenValue}`
+      `${URL}/checklists/${checkListId}?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error deleting the checklist!", error.message);
     throw error;
   }
 }
@@ -151,11 +138,10 @@ export async function deleteCheckList(checkListId) {
 export async function getCheckItems(checkListId) {
   try {
     const response = await axios.get(
-      `https://api.trello.com/1/checklists/${checkListId}/checkItems?key=${keyValue}&token=${tokenValue}`
+      `${URL}/checklists/${checkListId}/checkItems?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error getting the check items!", error.message);
     throw error;
   }
 }
@@ -163,11 +149,10 @@ export async function getCheckItems(checkListId) {
 export async function createCheckItem(checkListId, checkItemName) {
   try {
     const response = await axios.post(
-      `https://api.trello.com/1/checklists/${checkListId}/checkItems?name=${checkItemName}&key=${keyValue}&token=${tokenValue}`
+      `${URL}/checklists/${checkListId}/checkItems?name=${checkItemName}&key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error creating the check item!", error.message);
     throw error;
   }
 }
@@ -175,11 +160,10 @@ export async function createCheckItem(checkListId, checkItemName) {
 export async function deleteCheckItem(checkListId, idCheckItem) {
   try {
     const response = await axios.delete(
-      `https://api.trello.com/1/checklists/${checkListId}/checkItems/${idCheckItem}?key=${keyValue}&token=${tokenValue}`
+      `${URL}/checklists/${checkListId}/checkItems/${idCheckItem}?key=${keyValue}&token=${tokenValue}`
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error deleting the check item!", error.message);
     throw error;
   }
 }
@@ -187,12 +171,11 @@ export async function deleteCheckItem(checkListId, idCheckItem) {
 export async function changeItemCheckbox(cardId, checkItemId, newState) {
   try {
     const response = await axios.put(
-      `https://api.trello.com/1/cards/${cardId}/checkItem/${checkItemId}?key=${keyValue}&token=${tokenValue}`,
+      `${URL}/cards/${cardId}/checkItem/${checkItemId}?key=${keyValue}&token=${tokenValue}`,
       { state: newState }
     );
     return checkStatus(response);
   } catch (error) {
-    console.error("Error changing the checkbox state!", error.message);
     throw error;
   }
 }
