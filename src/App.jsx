3ae9860f0edfae021fe2c4f430176e9481093327
YadavDipak <yadavdipak2911@gmail.com>
2025-01-31 23:37:47 +0530

    App file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Content from './components/HomePage/Content'
-import NavBar from './components/NavBar'
-import BoardInfo from './components/BoardView/BoardInfo'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Content from "./components/HomePage/Content";
+import MainLayout from "./layout/MainLayout";
+import BoardInfo from "./components/BoardView/BoardInfo";
 
 function App() {
   return (
     <>
-      <NavBar />
       <Routes>
         <Route path="/" element={<Navigate to="/boards" />} />
-        <Route path="/boards" element={<Content />} />
-        <Route path="/boards/:id" element={<BoardInfo />}></Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/boards" element={<Content />} />
+          <Route path="/boards/:id" element={<BoardInfo />}></Route>
+        </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
