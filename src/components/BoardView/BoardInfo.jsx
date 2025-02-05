155108c565144b7a7da316a512cfaa632afa7df4
YadavDipak <yadavdipak2911@gmail.com>
2025-02-05 14:56:15 +0530

    Boards information updated

--- src/components/BoardView/BoardInfo.jsx
+++ src/components/BoardView/BoardInfo.jsx
@@ -2,16 +2,15 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box, Button } from "@mui/material";
 import { getAllBoard, createLists, FetchApi } from "../../services/FetchApi";
-import Lists from '../ListView/Lists'
+import Lists from "../ListView/Lists";
 import DialogBox from "../DialogBox";
-
 
 function BoardInfo() {
   const { id } = useParams();
   const [lists, setLists] = useState([]);
-  const [boardName, setBoardName] = useState('');
+  const [boardName, setBoardName] = useState("");
   const [open, setOpen] = useState(false);
-  const [listName, setListName] = useState('');
+  const [listName, setListName] = useState("");
 
   useEffect(() => {
     FetchApi(id)
@@ -24,11 +23,9 @@ function BoardInfo() {
       .then((boards) => {
         const board = boards.find((board) => board.id === id);
         setBoardName(board.name);
-
       })
       .catch((err) => console.log("Error fetching board details:", err));
   }, [id]);
-
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -45,36 +42,55 @@ function BoardInfo() {
       })
       .catch((err) => console.log(err));
     handleClose();
-    setListName('')
+    setListName("");
   }
 
   function listDisposed(deletedlist) {
-    const filteredLists = lists.filter((list) => list.id !== deletedlist.id)
+    const filteredLists = lists.filter((list) => list.id !== deletedlist.id);
     setLists(filteredLists);
   }
 
-
   return (
     <>
-      <Box sx={{ fontSize: '2rem', fontWeight: 'bold', padding: 2, marginLeft: 0.5 }}> {boardName} </Box>
-      <Box component="span" sx={{ display: 'flex', overflowX: 'scroll', height: '100vh' }} >
-        <Box sx={{ display: 'flex' }}>
-
-          {lists.length > 0 ? (lists.map((list) => (
-            <Lists key={list.id} handleListChange={listDisposed} listInfo={list} />
-          ))) : (<Box sx={{ width: '10vw', m: 2 }}>Loading Lists...</Box>)}
+      <Box
+        sx={{
+          fontSize: "2rem",
+          fontWeight: "bold",
+          padding: 2,
+          marginLeft: 0.5,
+        }}
+      >
+        {" "}
+        {boardName}{" "}
+      </Box>
+      <Box
+        component="span"
+        sx={{ display: "flex", overflowX: "scroll", height: "100vh" }}
+      >
+        <Box sx={{ display: "flex" }}>
+          {lists.length > 0 ? (
+            lists.map((list) => (
+              <Lists
+                key={list.id}
+                handleListChange={listDisposed}
+                listInfo={list}
+              />
+            ))
+          ) : (
+            <Box sx={{ width: "10vw", m: 2 }}>Loading Lists...</Box>
+          )}
 
           <Button
             onClick={handleOpen}
             variant="outlined"
             sx={{
               m: 2,
-              width: '20vw',
-              height: '10vh',
+              width: "20vw",
+              height: "10vh",
               color: "#ffff",
               backgroundColor: "#4682B4",
               border: "2px solid #f0f0f0",
-              fontWeight: 'bold'
+              fontWeight: "bold",
             }}
           >
             + Add a List
@@ -87,7 +103,7 @@ function BoardInfo() {
         handleClose={handleClose}
         handleSubmit={handleFormSubmit}
         textValue={listName}
-        title='Create List'
+        title="Create List"
         textChange={handleListName}
       />
     </>
